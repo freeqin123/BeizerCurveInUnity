a0f875adbeab8e71c795926edaf2fe85820f4dd9
FREEqin <33226894+freeqin123@users.noreply.github.com>
2019-07-21 11:49:28 +0800

    第二次提交
    研究了一下SplineMesh，感觉不是很好用，尤其这种东西是在编辑器环境下运行的，不清楚，渲染mesh的脚本警告不要更改node节点，但是其封装的方法中并没有明确的方法可以对node的List做出修改，目前提交并未对访问节点然后对其进行修改进行测试！

---
+++ Assets/MyBeizerLine/Editor/LineInspector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Lines))]
+public class LineInspector : Editor
+{
+    private void OnSceneGUI()
+    {
+        Lines line = target as Lines;
+        Transform handleTransform = line.transform;
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        Handles.color = Color.blue;
+        Handles.DrawLine(p0,p1);
+        Handles.DoPositionHandle(p0,handleRotation);
+        Handles.DoPositionHandle(p1,handleRotation);
+
+        EditorGUI.BeginChangeCheck();
+        p0 = Handles.DoPositionHandle(p0,handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line,"Move Point");
+            EditorUtility.SetDirty(line);
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1,handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+    }
+}
